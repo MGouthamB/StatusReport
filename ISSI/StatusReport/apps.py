7b6f3f78217733adafc002707efef84a3fdf6bc0
goutham <30994768+MGouthamB@users.noreply.github.com>
2024-06-25 14:10:45 -0400

    Login Page Setup
    1. Included the status report application in the INSTALLED_APPS.
    2. Created a directory for templates and add the path to templates.
    3. Created urls.py for statusReport app and included in the main url page.
    4. Renamed the statusReport folder to StatusReport (naming conventions).

--- ISSI/StatusReport/apps.py
+++ ISSI/StatusReport/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StatusreportConfig(AppConfig):
+class StatusReportConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "statusReport"
+    name = "StatusReport"
