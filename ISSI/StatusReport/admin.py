43ed4cd17cd8105868a2dd363cdc08c4794bff78
Manikanta-2000 <manikantaganti@gmail.com>
2024-06-26 07:38:03 -0400

    Added Tasks,group and user model

--- ISSI/StatusReport/admin.py
+++ ISSI/StatusReport/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Employee,Groups,Tasks
 
 # Register your models here.
+admin.site.register()
--- ISSI/StatusReport/models.py
+++ ISSI/StatusReport/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
-# Create your models here.
+
+## group table - which stores project teams  in an organization
+class Groups(models.Model):
+    name = models.TextField(unique=True)
+
+## tasks table - which  store reports or tasks of a user 
+class Tasks(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    group = models.ForeignKey(Groups,on_delete=models.CASCADE)
+    name= models.TextField()
+    date=models.DateField(default=date.today())
+    completed = models.BooleanField(default=False)
+
+    # class Meta:
+    #     permissions=(
+    #         ("view_all_reports","can view reports of all the users"),
+    #         ("view_project_reports","can view reports of all users for a particular project")
+    #     )
+    
+## employee table with defuatl user fields and added 2 extra fields
+class Employee(models.Model):
+    ACCOUNT_TYPES={
+        "SA":"System Administrator",
+        "PM":"Project Manager",
+        "M":"Project Member"
+    }
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    groups=models.TextField()
+    account_type=models.CharField(max_length=3,choices=ACCOUNT_TYPES,default="M")
